a36f642b7e5357c3e8c2625f8ac79c0f16d6b757
tristob <tristan.bardey@gmail.com>
2014-12-05 06:25:48 +0100

    home page almost done

--- unPeuDang.js
+++ unPeuDang.js
@@ -13,4 +13,10 @@ app.controller("themeController", function () {
 
     ];
 
-})
+});
+app.controller("cause1Controller", function(){
+    this.lescause1=[{nom:"Du Foutre!", description:"J'aime le foutre, la bite, les cul et enfant, mais pas plus de 10ans.", img:"http://41.media.tumblr.com/fd1c27a2a95c6f54cf4d3d87c7c5e245/tumblr_n8ux6khCBf1s90ewoo1_250.jpg"}
+
+    ];
+
+});
