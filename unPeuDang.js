dff48a2a7b7d4c59902bd92d0a925044ce2f0d7a
tristob <tristan.bardey@gmail.com>
2014-12-05 02:14:52 +0100

    on passe en angular dans pour les themes

---
+++ unPeuDang.js
@@ -0,0 +1,9 @@
+/**
+ * Created by Tristan on 05/12/2014.
+ */
+var app = angular.module('themes',[]);
+
+app.controller("themeController", function () {
+    this.lesthemes = [{nom : "Famine"},{nom : "PD"}];
+
+})
